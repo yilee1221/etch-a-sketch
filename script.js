7b74e22a7760c6f9d64376f87c63d0cc839aa2e6
Erin Lee <123915538+yilee1221@users.noreply.github.com>
2023-04-20 13:52:18 +0900

    initial grid

---
+++ script.js
@@ -0,0 +1,12 @@
+const grid = document.querySelector(".grid");
+grid.style.gridTemplateRows = `repeat(16, 1fr)`;
+grid.style.gridTemplateColumns = `repeat(16, 1fr)`;
+for (let i=0; i < 16; i++) {
+    for (let j=0; j < 16; j++) {
+        const square = document.createElement("div");
+        square.classList.add('squares');
+        square.style.width = "40px";
+        square.style.height = "40px";
+        grid.appendChild(square);
+    }
+}
