d31fa30739a294bb61114c2fa38c5d0ee441be8e
Erin Lee <123915538+yilee1221@users.noreply.github.com>
2023-05-15 11:06:00 +0900

    upgraded grid and color change

--- script.js
+++ script.js
@@ -1,20 +1,47 @@
 const grid = document.querySelector(".grid");
 grid.style.gridTemplateRows = `repeat(16, 1fr)`;
 grid.style.gridTemplateColumns = `repeat(16, 1fr)`;
+const gridSize = 640;
 for (let i=0; i < 16; i++) {
     for (let j=0; j < 16; j++) {
         const square = document.createElement("div");
-        square.classList.add('squares');
+        square.classList.add('square');
         square.style.width = "40px";
         square.style.height = "40px";
+        square.hasAttribute
         grid.appendChild(square);
     }
 }
-function filled() {
-    this.classList.add('fill');
+let color = "#000000"; 
+const colorPicker = document.getElementById("pickColor");
+colorPicker.addEventListener("input", () => {
+    color = colorPicker.value;
+})
+function fillColor() {
+    if(this.style.backgroundColor == "" && mouseDown)
+    this.style.backgroundColor = color;
 }
-const pixels = document.querySelectorAll('.squares');
+let mouseDown = false;
+const pixels = document.querySelectorAll('.square');
 pixels.forEach((square) => {
-    square.addEventListener('mouseover', filled);
+    square.addEventListener('mousedown', () => {
+        if(square.style.backgroundColor == "")
+            square.style.backgroundColor = color;
+    }); 
+    square.addEventListener('mouseover', fillColor);
+});
+
+
+
+grid.addEventListener('mousedown', () => {
+    mouseDown = true;
+});
+
+grid.addEventListener("mouseup", () => {
+    mouseDown = false;
+});
+
+grid.addEventListener("mouseleave", () => {
+    mouseDown = false;
 });
 
