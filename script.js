30deb1f00b059c05fc5d782abe923e93d12da6fe
Erin Lee <123915538+yilee1221@users.noreply.github.com>
2023-05-16 12:41:06 +0900

    update grid size

--- script.js
+++ script.js
@@ -1,47 +1,59 @@
+const gridSize = 16;
 const grid = document.querySelector(".grid");
-grid.style.gridTemplateRows = `repeat(16, 1fr)`;
-grid.style.gridTemplateColumns = `repeat(16, 1fr)`;
-const gridSize = 640;
-for (let i=0; i < 16; i++) {
-    for (let j=0; j < 16; j++) {
-        const square = document.createElement("div");
-        square.classList.add('square');
-        square.style.width = "40px";
-        square.style.height = "40px";
-        square.hasAttribute
-        grid.appendChild(square);
-    }
-}
-let color = "#000000"; 
-const colorPicker = document.getElementById("pickColor");
-colorPicker.addEventListener("input", () => {
-    color = colorPicker.value;
-})
-function fillColor() {
-    if(this.style.backgroundColor == "" && mouseDown)
-    this.style.backgroundColor = color;
-}
 let mouseDown = false;
-const pixels = document.querySelectorAll('.square');
-pixels.forEach((square) => {
-    square.addEventListener('mousedown', () => {
-        if(square.style.backgroundColor == "")
-            square.style.backgroundColor = color;
-    }); 
-    square.addEventListener('mouseover', fillColor);
-});
-
-
-
+// Check mouse down for click and drag to draw
 grid.addEventListener('mousedown', () => {
     mouseDown = true;
 });
-
 grid.addEventListener("mouseup", () => {
     mouseDown = false;
 });
-
 grid.addEventListener("mouseleave", () => {
     mouseDown = false;
 });
 
+// Make grid and draw
+function updateGrid(size) {
+    let newSize = size;
+    grid.innerHTML = "";
+    console.log(grid)
+    grid.style.gridTemplateRows = `repeat(${newSize}, 1fr)`;
+    grid.style.gridTemplateColumns = `repeat(${newSize}, 1fr)`;
+    for (let i=0; i < newSize; i++) {
+        for (let j=0; j < newSize; j++) {
+            const square = document.createElement("div");
+            square.classList.add('square');
+            grid.appendChild(square);
+        }
+    }
+    let pixels = document.querySelectorAll('.square');
+    pixels.forEach((square) => {
+        square.addEventListener('mousedown', () => {
+            if(square.style.backgroundColor == "")
+                square.style.backgroundColor = color;
+        }); 
+        square.addEventListener('mouseover', fillColor);
+    });
+}
+
+// Get input for grid size and update grid
+const inputSize = document.querySelector('.inputSize');
+const displaySize = document.querySelector('#size');
+displaySize.textContent = inputSize.value;
+inputSize.addEventListener("input",() => {
+    displaySize.textContent = inputSize.value;
+    updateGrid(inputSize.value);
+});
+//Get input for color
+let color = "#000000"; 
+const colorPicker = document.getElementById("pickColor");
+colorPicker.addEventListener("input", () => {
+    color = colorPicker.value;
+});
+// fill in squares if not filled in and mouse is down 
+function fillColor() {
+    if(this.style.backgroundColor == "" && mouseDown)
+        this.style.backgroundColor = color;
+}
+
+updateGrid(gridSize);
