764f549a2a0545215d553b4d26be02004302df84
Erin Lee <123915538+yilee1221@users.noreply.github.com>
2023-05-17 15:55:28 +0900

    edited format with eraser and clear

--- script.js
+++ script.js
@@ -12,28 +12,48 @@ grid.addEventListener("mouseleave", () => {
     mouseDown = false;
 });
 
+let erase = false;
+const eraserButton = document.querySelector(".eraser");
+const clearButton = document.querySelector(".clear");
+
 // Make grid and draw
 function updateGrid(size) {
-    let newSize = size;
+    let newSize = size
     grid.innerHTML = "";
-    console.log(grid)
     grid.style.gridTemplateRows = `repeat(${newSize}, 1fr)`;
     grid.style.gridTemplateColumns = `repeat(${newSize}, 1fr)`;
     for (let i=0; i < newSize; i++) {
         for (let j=0; j < newSize; j++) {
             const square = document.createElement("div");
             square.classList.add('square');
+            square.style.backgroundColor = "";
             grid.appendChild(square);
         }
     }
     let pixels = document.querySelectorAll('.square');
     pixels.forEach((square) => {
         square.addEventListener('mousedown', () => {
-            if(square.style.backgroundColor == "")
+            if(square.style.backgroundColor == "" && !erase)
                 square.style.backgroundColor = color;
         }); 
         square.addEventListener('mouseover', fillColor);
     });
+
+    eraserButton.addEventListener("click", () => erase = true);
+
+    pixels.forEach((square) => {
+        square.addEventListener('mousedown', () => {
+            if (erase) square.style.backgroundColor = "";
+        }); 
+        square.addEventListener('mouseover', eraseColor);
+        //square.addEventListener('mouseup', () => erase = false);
+    });
+
+    clearButton.addEventListener("click", () => {
+        pixels.forEach((square) => {
+            square.style.backgroundColor = "";
+        })
+    })
 }
 
 // Get input for grid size and update grid
@@ -50,10 +70,20 @@ const colorPicker = document.getElementById("pickColor");
 colorPicker.addEventListener("input", () => {
     color = colorPicker.value;
 });
-// fill in squares if not filled in and mouse is down 
+colorPicker.addEventListener("click", () => erase = false);
+
+// fill in squares if not filled in and mouse is down and not eraser
 function fillColor() {
-    if(this.style.backgroundColor == "" && mouseDown)
+    if(this.style.backgroundColor == "" && mouseDown && !erase)
         this.style.backgroundColor = color;
+}
+// erase color when mouse is down after clicking eraser
+function eraseColor() {
+    if(mouseDown && erase)
+        this.style.backgroundColor = "";
 }
 
 updateGrid(gridSize);
+
+
+
